f81439896e8a2c7726a806eed43f4737940a9ffd
Arjo Bruijnes <arjo@weirdwater.net>
2020-02-20 09:54:30 +0100

    Restructure the string value splitting as a pipe

--- fp-crypto/src/mod10.ts
+++ fp-crypto/src/mod10.ts
@@ -1,3 +1,4 @@
+import { pipe } from "fp-ts/lib/pipeable"
 
 const def = <a>(x: a | undefined): x is a => typeof x !== 'undefined'
 
@@ -5,11 +6,9 @@ const undef = <a>(x: a | undefined): x is undefined => !def(x)
 
 const map = <a, b>(f: (x: a) => b) => ([x, ...xs]: a[]): b[] => undef(x) ? [] : [f(x), ...map(f)(xs)]
 
+const filter = <a>(p: (x: a) => boolean) => ([x, ...xs]: a[]): a[] => def(x) ? p(x) ? [x, ...filter<a>(p)(xs)] : filter<a>(p)(xs) : []
+
 const flatten = <a>([x, ...xs]: Array<a|a[]>): a[] => def(x) ? Array.isArray(x) ? [...x, ...flatten(xs)] : [x, ...flatten(xs)] : []
-
-const parseInts = map((x: string) => parseInt(x, 10))
-
-const splitInts = map((x: number) => x.toString(10).split(""))
 
 export const addAndMod10 = (a: number, b: number) => (a + b) % 10
 
@@ -20,10 +19,13 @@ export const mod10Array = (a: number[]): number[] => a
 
 export const mod10 = (s: string): string => {
 
-  const digits = parseInts(flatten(splitInts(alphaValues(s))))
-
-
-
+  const digits = pipe(
+    alphaValues(s),
+    // filter(x => x !== 32), // filter out spaces (Should this be done inside the hashing function?)
+    map(x => x.toString(10).split("")),
+    flatten,
+    map(x => parseInt(x, 10))
+  )
 
   return s
 }
