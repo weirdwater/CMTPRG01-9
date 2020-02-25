0b8624b316b96c28141db0228c26ef5790042d7d
Arjo Bruijnes <arjo@weirdwater.net>
2020-02-25 23:37:36 +0100

    Add CMGT coin types

---
+++ fp-crypto/src/types.ts
@@ -0,0 +1,42 @@
+
+export interface TransactionData {
+  _id: string
+  from: string
+  to: string
+  amount: number
+  timestamp: number
+}
+
+export type Transaction = TransactionData & { __v: number }
+
+export interface Block {
+  _id: string
+  algorithm: string
+  hash: string
+  nonce: string
+  timestamp: number
+  __v: 0
+  data: TransactionData[]
+}
+
+export interface NextOpen {
+  blockchain: Block
+  transactions: Transaction[]
+  timestamp: number
+  algorith: string
+  open: true
+  countdown: number
+}
+
+export interface NextClosed {
+  open: false
+  message: string
+  countdown: number
+}
+
+export type Next = NextOpen | NextClosed
+
+export interface SolutionDTO {
+  nonce: string
+  user: string
+}
