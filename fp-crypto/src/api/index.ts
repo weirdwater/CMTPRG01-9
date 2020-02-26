adf2ddf4f06f0945bc1d1d50c813d8d90ce21cb3
Arjo Bruijnes <arjo@weirdwater.net>
2020-02-26 12:36:11 +0100

    Start working on api

---
+++ fp-crypto/src/api/index.ts
@@ -0,0 +1,4 @@
+import { Next } from "./types";
+
+const baseUrl = 'https://programmeren9.cmgt.hr.nl:8000/api/blockchain'
+
--- fp-crypto/src/main.ts
+++ fp-crypto/src/main.ts
@@ -1,9 +1,2 @@
-import { supplement } from "./mod10"
 
-console.log("hello world")
-
-
-export const add = (a: number) => (b: number) => a + b
-
-
-console.log(supplement([1, 6]))
+console.log('nothing to see here')
