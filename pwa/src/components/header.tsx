c38e0d69b6f8f92c459669d28fa4641eba85be46
Arjo Bruijnes <arjo@weirdwater.net>
2020-03-16 19:24:19 +0100

    Add header component

---
+++ pwa/src/components/header.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as styles from '../style/components/header.scss'
+
+export const Header = () => <header className={styles.header} >
+  <h1 className={styles.title} >CMGT Showcase</h1>
+</header>
