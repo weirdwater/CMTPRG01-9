3be717e46a74fe563aae8997db53abac24d9cdb8
Arjo Bruijnes <arjo@weirdwater.net>
2020-04-14 23:37:27 +0200

    Correct GH pages location

--- pwa/webpack.config.js
+++ pwa/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src/main.tsx'),
   output: {
-    path: path.resolve(__dirname, '..', 'doc'),
+    path: path.resolve(__dirname, '..', 'docs'),
     filename: 'showcase.bundle.js'
   },
   resolve: {
