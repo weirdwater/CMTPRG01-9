c9ea5ba30c45c5b5dba1138b35d28ff520d737fd
Arjo Bruijnes <arjo@weirdwater.net>
2020-04-14 18:36:44 +0200

    Make header direct user to the root

--- pwa/src/components/header.tsx
+++ pwa/src/components/header.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as styles from '../style/components/header.scss'
+import { Link } from 'react-router-dom'
 
 export const Header = () => <header className={styles.header} >
-  <h1 className={styles.title} >CMGT Showcase</h1>
+  <h1 className={styles.title} ><Link to='/' className={styles.link}>CMGT Showcase</Link></h1>
 </header>
--- pwa/src/style/components/header.scss.d.ts
+++ pwa/src/style/components/header.scss.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly "header": string;
+  readonly "link": string;
   readonly "title": string;
 };
 export = styles;
